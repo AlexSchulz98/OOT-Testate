79083fce0c58e67635fff7ec1e955a442e545c9a
jensk <jensk@DESKTOP-5CIJ1OR.Speedport_W_724V_01011603_05_020>
2017-04-19 18:25:02 +0200

    OOT Zeugs

---
+++ oot aufgabe 1/src/Ast.java
@@ -0,0 +1,12 @@
+
+public class Ast {
+int inhalt;
+Ast links;
+Ast rechts;
+Ast(int wert){
+	this.inhalt=wert;
+	this.links=null;
+	this.rechts=null;
+	
+}
+}
---
+++ oot aufgabe 1/src/Testbaum.java
@@ -0,0 +1,9 @@
+
+public class Testbaum {
+
+	public static void main(String[] args) {
+		Binaer_Baum baum=new Binaer_Baum();
+baum.einfuegem(3);
+	}
+
+}
